eb02ac0c6c1260c9230eacbe0ce0a024c2e08ecd
linda-panda <fang.zhuoma@gmail.com>
2016-04-18 00:54:16 +1000

    Add contact us page, useful tips page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+function init(){
 
+	var timeDiff = document.getElementById("timeDiff");
+	var now = new Date()
+	var localTime = now.getTime();
+	// var date = new Date(localTime);
+	// var u = alter(date.toUTCString());
+	var hour = now.getHours();
+	var min = now.getMinutes();
+	var localoffset = now.getTimezoneOffset();
+
+	//Tibet time is GMT+8, so it is -480 minutes in getTimezoneOffset() format; 
+	if (localoffset<-480){
+		var hourDiff= (-480-localoffset)/60;
+		var message = "Tibet is " + hourDiff + "hour(s) behind your time";
+	}else{
+		hourDiff = (-480-localoffset)/60;
+		var message = "Tibet is " + hourDiff + "hour(s) ahead your time";
+	}		
+	timeDiff.innerHTML="Hi There!"+"<br>" + " Your local time is "+ now +"<br>";
+	timeDiff.innerHTML+= message;
+	
+}
+window.onload = init;
