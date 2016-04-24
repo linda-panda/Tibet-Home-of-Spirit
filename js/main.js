ef7f6d962b4ef171e13000e9f958477f127096fd
linda-panda <fang.zhuoma@gmail.com>
2016-04-24 22:07:37 +1000

    Validated code, including CSS and HTML

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+//time difference caulculation on Useful Tips page
 function init(){
-
 	var timeDiff = document.getElementById("timeDiff");
 	var now = new Date()
 	var localTime = now.getTime();
@@ -12,13 +12,27 @@ function init(){
 	//Tibet time is GMT+8, so it is -480 minutes in getTimezoneOffset() format; 
 	if (localoffset<-480){
 		var hourDiff= (-480-localoffset)/60;
-		var message = "Tibet is " + hourDiff + "hour(s) behind your time";
+		var message = "Tibet is " + hourDiff + " hour(s) behind your time";
 	}else{
 		hourDiff = (-480-localoffset)/60;
 		var message = "Tibet is " + hourDiff + "hour(s) ahead your time";
 	}		
-	timeDiff.innerHTML="Hi There!"+"<br>" + " Your local time is "+ now +"<br>";
+	timeDiff.innerHTML="Your local time is "+ now +"<br>";
 	timeDiff.innerHTML+= message;
-	
 }
 window.onload = init;
+
+//form validation on contact us page
+
+function validateForm(){
+	var emailInfo = document.getElementById("email").value;
+	var lastAtPos = emailInfo.lastIndexOf('@');
+	var lastDotPos = emailInfo.lastIndexOf('.');
+	if (lastAtPos < lastDotPos && lastAtPos > 0 && emailInfo.indexOf('@@') == -1 && lastDotPos > 2 && (emailInfo.length - lastDotPos) > 2) {
+		alert("Contact form has been submitted");
+	}
+	else {
+		alert("please enter a valid email");
+	}
+}
+
